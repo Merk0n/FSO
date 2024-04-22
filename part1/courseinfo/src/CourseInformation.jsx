c65b78e9e87bb4c774ffd3b9d5d8ef6a3ab9bcb4
merk0n <127611734+Merk0n@users.noreply.github.com>
2024-04-22 21:50:50 +0200

    part1-courseinformation-step-3-5

---
+++ part1/courseinfo/src/CourseInformation.jsx
@@ -0,0 +1,53 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{' '}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const CourseInformation = ({ course, parts }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content
+        part1={parts[0].name}
+        exercises1={parts[0].exercises}
+        part2={parts[1].name}
+        exercises2={parts[1].exercises}
+        part3={parts[2].name}
+        exercises3={parts[2].exercises}
+      />
+      <Total
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
+      />
+    </>
+  );
+};
+
+export default CourseInformation;
